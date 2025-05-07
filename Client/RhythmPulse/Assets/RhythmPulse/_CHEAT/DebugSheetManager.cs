de1a95714b5d8d53cf8c6646bcb1db79c9c58aab
Mai Kuraki <mai.k@live.com>
2025-05-07 19:43:09 +0800

    Add Cheat Module

---
+++ Client/RhythmPulse/Assets/RhythmPulse/_CHEAT/DebugSheetManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Reflection;
+using IngameDebugConsole;
+using UnityDebugSheet.Runtime.Core.Scripts;
+using UnityDebugSheet.Runtime.Extensions.IngameDebugConsole;
+using UnityEngine;
+
+namespace RhythmPulse.Cheat
+{
+    public class DebugSheetManager : MonoBehaviour
+    {
+        private DebugPage debugPageRoot;
+
+        private void Awake()
+        {
+            debugPageRoot = DebugSheet.Instance.GetOrCreateInitialPage();
+
+            debugPageRoot.AddButton("Toggle FPS", clicked: () =>
+            {
+                IsFPSVisible = !IsFPSVisible;
+                SetFPSVisibility(IsFPSVisible);
+            });
+            debugPageRoot.AddPageLinkButton<IngameDebugConsoleDebugPage>("In-Game Console Debug", onLoad: x => x.page.Setup(DebugLogManager.Instance));
+        }
+
+        private bool IsFPSVisible = false;
+        public void SetFPSVisibility(bool isVisible)
+        {
+            try
+            {
+                Type fpsType = Type.GetType("CycloneGames.Utility.Runtime.FPSCounter, Assembly-CSharp");
+
+                if (fpsType == null)
+                {
+                    fpsType = Type.GetType("CycloneGames.Utility.Runtime.FPSCounter, CycloneGames.Utility.Runtime");
+
+                    if (fpsType == null)
+                    {
+                        Debug.LogError("FPSCounter type not found");
+                        return;
+                    }
+                }
+                UnityEngine.Object[] fpsCounters = UnityEngine.Object.FindObjectsByType(fpsType, FindObjectsSortMode.None);
+
+                if (fpsCounters.Length == 0)
+                {
+                    Debug.LogWarning("No FPSCounter instances found");
+                    return;
+                }
+                FieldInfo field = fpsType.GetField("IsVisible",
+                    BindingFlags.Public | BindingFlags.Instance);
+
+                if (field == null)
+                {
+                    Debug.LogError("IsVisible field not found");
+                    return;
+                }
+                foreach (UnityEngine.Object counter in fpsCounters)
+                {
+                    field.SetValue(counter, isVisible);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError($"Reflection failed: {ex}");
+            }
+        }
+    }
+}
