a7131676b4f0e7632967d6dd5b0459ab910ea255
Mai Kuraki <mai.k@live.com>
2026-01-01 02:59:31 +0800

    Fix DebugSheet error for Unity 6000.3

---
+++ Client/UnityPackages/CycloneGames/com.harumak.unitydebugsheet/Runtime/Foundation/Drawer/CanvasGroupDrawerBackdrop.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace UnityDebugSheet.Runtime.Foundation.Drawer
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public sealed class CanvasGroupDrawerBackdrop : DrawerBackdrop
+    {
+        private CanvasGroup _canvasGroup;
+
+        private void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void SetProgressInternal(float visibility)
+        {
+            _canvasGroup.alpha = visibility;
+        }
+
+        protected override void SetInteractable(bool interactable)
+        {
+            _canvasGroup.interactable = interactable;
+        }
+    }
+}
